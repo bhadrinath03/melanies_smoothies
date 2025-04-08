6200a0e0079bf21a4966e1f9640bb43378e44be3
bhadrinath03 <bhadrinath0320@gmail.com>
2025-04-08 19:29:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,', icon="✅")
-cnx=st.connection("snowflake")
-session=cnx.session()
